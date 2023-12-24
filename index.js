b13c6466dd2f948c261cc793a477f0e19bc1393d
rakibulapon46 <rakibulapon46@gmail.com>
2023-12-25 00:34:30 +0600

    first commit

--- index.js
+++ index.js
@@ -54,102 +54,28 @@ function createCarousel(carouselId, prevButtonId, nextButtonId) {
 createCarousel('carousel-slide1', 'prev1', 'next1');
 createCarousel('carousel-slide2', 'prev2', 'next2');
 
-// const carousel = document.getElementById('carousel-slide');
-// const prevButton = document.getElementById('prev');
-// const nextButton = document.getElementById('next');
 
-// let currentIndexNo = 0;
-// let translateX = 0;
+// Tab section 
 
-// // Autoplay configuration
-// const autoplayInterval = 3000; // 3 seconds
-// let autoplayTimer;
-
-// function startAutoplay() {
-//   autoplayTimer = setInterval(() => {
-//     showNext();
-//   }, autoplayInterval);
-// }
-
-// function stopAutoplay() {
-//   clearInterval(autoplayTimer);
-// }
-
-// function updateCarousel() {
-//   carousel.style.transform = `translateX(${translateX}px)`;
-// }
-
-// function showPrev() {
-//   currentIndexNo = (currentIndexNo - 1 + carousel.children.length) % carousel.children.length;
-//   translateX = -currentIndexNo * carousel.clientWidth;
-//   updateCarousel();
-// }
-
-// function showNext() {
-//   currentIndexNo = (currentIndexNo + 1) % carousel.children.length;
-//   translateX = -currentIndexNo * carousel.clientWidth;
-//   updateCarousel();
-// }
-
-// // Attach event listeners
-// prevButton.addEventListener('click', () => {
-//   stopAutoplay();
-//   showPrev();
-// });
-
-// nextButton.addEventListener('click', () => {
-//   stopAutoplay();
-//   showNext();
-// });
-
-// // Start autoplay
-// startAutoplay();
-
-
-// const slides = document.querySelectorAll(".slide");
-// let counter = 1;
-// // console.log(slides);
-// slides.forEach((slide, index) => {
-//   slide.style.left = `${index * 100}%`;
-// });
-
-// const goPrev = () => {
-//   counter--;
-//   slideImage();
-// };
-
-// const goNext = () => {
-//   counter++;
-//   slideImage();
-// };
-
-// const slideImage = () => {
-//   slide.forEach((slide) => {
-//     slide.style.transform = `translateX(-${counter * 100}%)`;
-//   });
-// };
-
-
-
-var tabButtons = document.querySelectorAll(".tablinks");
+let tabButtons = document.querySelectorAll(".tablinks");
 
 function updateTabStyles() {
   // Get the window width
-  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
   // Loop through each tab button
-  for (var i = 0; i < tabButtons.length; i++) {
+  for (let i = 0; i < tabButtons.length; i++) {
     tabButtons[i].addEventListener("click", function () {
-      var tabName = this.dataset.tab;
-      var tabContent = document.getElementById(tabName);
-      var allTabContent = document.querySelectorAll(".tabcontent");
-      var allTabButtons = document.querySelectorAll(".tablinks");
+      let tabName = this.dataset.tab;
+      let tabContent = document.getElementById(tabName);
+      let allTabContent = document.querySelectorAll(".tabcontent");
+      let allTabButtons = document.querySelectorAll(".tablinks");
 
-      for (var j = 0; j < allTabContent.length; j++) {
+      for (let j = 0; j < allTabContent.length; j++) {
         allTabContent[j].style.display = "none";
       }
 
-      for (var k = 0; k < allTabButtons.length; k++) {
+      for (let k = 0; k < allTabButtons.length; k++) {
         allTabButtons[k].classList.remove("active");
       }
 
@@ -175,47 +101,6 @@ window.addEventListener('resize', updateTabStyles);
 document.querySelector(".tablinks").click();
 
 
-// const slideshowElements = document.querySelectorAll(".slideshow-element");
-
-// let countElements = 1;
-
-// setInterval(() => {
-//     countElements ++;
-//     let currentElement = document.querySelector(".current");
-
-//     currentElement.classList.remove("current");
-
-//     if (countElements > slideshowElements.length) {
-//         slideshowElements[0].classList.add("current");
-//         countElements = 1;
-//     } else{
-//         currentElement.nextElementSibling.classList.add("current");
-//     }
-// }, 2000);
-
-var tabButtons = document.querySelectorAll(".tablinks");
-
-    for (var i = 0; i < tabButtons.length; i++) {
-      tabButtons[i].addEventListener("click", function () {
-        var tabName = this.dataset.tab;
-        var tabContent = document.getElementById(tabName);
-        var allTabContent = document.querySelectorAll(".tabcontent");
-        var allTabButtons = document.querySelectorAll(".tablinks");
-
-        for (var j = 0; j < allTabContent.length; j++) {
-          allTabContent[j].style.display = "none";
-        }
-
-        for (var k = 0; k < allTabButtons.length; k++) {
-          allTabButtons[k].classList.remove("active");
-        }
-        
-        tabContent.style.display = "flex";
-        this.classList.add("active");
-      });
-    }
-
-    document.querySelector(".tablinks").click();
 
 // card slider 
 
