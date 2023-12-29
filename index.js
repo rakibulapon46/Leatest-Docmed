cb69a5dec666f9fc63076dc705ee3963f3823e9e
rakibulapon46 <rakibulapon46@gmail.com>
2023-12-29 13:12:32 +0600

    first commit

--- index.js
+++ index.js
@@ -137,3 +137,10 @@ startAutoplaySys();
 prevBtn.addEventListener("click", handlePrev);
 nextBtn.addEventListener("click", handleNext);
 
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
